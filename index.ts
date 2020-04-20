7add5012ad649aee4ad027cd2699bf22cf4b3cb4
incetarik <incetarik@hotmail.com>
2020-04-20 17:43:09 +0300

    Added `createBot` and `IBotSettings`

---
+++ index.ts
@@ -0,0 +1,42 @@
+import Telegraf, { TelegrafOptions } from 'telegraf'
+
+import { WaitingStates } from './common'
+import { initEnvironment } from './environment'
+
+export interface IBotSettings extends TelegrafOptions {
+  /**
+   * The environment name of the token variable.
+   * `BOT_TOKEN` by default.
+   *
+   * @type {string}
+   * @memberof IBotSettings
+   */
+  tokenEnvName?: string
+
+  /**
+   * The name of the class function for catching errors.
+   * `onError` by default.
+   *
+   * When a function is found in this name inside of the class, the function
+   * will be used as error logger/handler.
+   *
+   * @type {string}
+   * @memberof IBotSettings
+   */
+  catchFunction?: string
+}
+
+export function createBot(opts: IBotSettings) {
+  initEnvironment()
+  const bot = new Telegraf(process.env[ opts.tokenEnvName || 'BOT_TOKEN' ] as string, opts)
+
+  bot.use((ctx, next) => {
+    if (!ctx.message) { next!(); return }
+    if (!ctx.from) { next!(); return }
+
+    WaitingStates.resolveFor(ctx.from.id, ctx.message)
+    next!()
+  })
+
+  return bot
+}
