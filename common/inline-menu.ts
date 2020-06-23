f97795e5201377630c277f85e8e12749c2ce8f34
incetarik <incetarik@hotmail.com>
2020-06-23 22:41:52 +0300

    Added generic type and documentation for `IInlineMenuInfo`

--- common/inline-menu.ts
+++ common/inline-menu.ts
@@ -35,21 +35,59 @@ export function inlineMenu(layout: IMenu | Func<IMenu | Promise<IMenu>, any>) {
  * Indicates whether an object is an inline menu indicator or not.
  *
  * @export
+ * @type {T} The type of the inline menu items.
  * @param {*} object Object to check.
  * @returns {IInlineMenuInfo} `true`
  * if the object is inline menu indicator object.
  */
-export function isInlineMenu(object: any): object is IInlineMenuInfo {
+export function isInlineMenu<T = any>(object: any): object is IInlineMenuInfo<T> {
   if (typeof object !== 'object') { return false }
   if (typeof object.inlineMenu === 'object') { return true }
   if (typeof object.inlineMenu === 'function') { return true }
   return false
 }
 
-export interface IInlineMenuInfo {
+export interface IInlineMenuInfo<T = any> {
+  /**
+   * The inline menu or the builder of it.
+   *
+   * @memberof IInlineMenuInfo
+   */
   inlineMenu: IMenu | Promise<IMenu> | MenuBuilder | ((resolve: (resolveWith: any) => void, reject: (e: Error) => void) => IMenu | Promise<IMenu>)
+
+  /**
+   * Indicates whether the menu should be closed on timeout or not.
+   *
+   * @type {boolean}
+   * @memberof IInlineOptionsInformation
+   */
   closeOnTimeout?: boolean
+
+  /**
+   * The message to be sent when timeout occurs.
+   *
+   * @type {string}
+   * @memberof IInlineOptionsInformation
+   */
   timeoutMessage?: string
-  onSelected?: Function
+
+  /**
+  * A function to be executed when an item is selected.
+  *
+  * Setting this will make the generator NON-BLOCKING.
+  *
+  * @type {Function}
+  * @memberof IInlineOptionsInformation
+  */
+  onSelected?(item: T): {
+    message ?: string;
+  } | void | undefined;
+
+  /**
+  * The extras of the template message.
+  *
+  * @type {ExtraEditMessage}
+  * @memberof IInlineOptionsInformation
+  */
   extra?: ExtraEditMessage
 }
